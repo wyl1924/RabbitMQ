6efc44e69dd9955eb1339c9eab0f4d31344114bc
kmonkey9006 <1714404171@qq.com>
2017-11-21 10:42:11 +0800

    http://www.cnblogs.com/kmonkeywyl/p/7866162.html

---
+++ RabbitMQ.RabbitMqConst/Base.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.RabbitMqConst
+{
+    public class Base
+    {
+        /// <summary>
+        /// 队列服务器路径
+        /// </summary>
+        public static string ServerAddress = ConfigurationManager.AppSettings["serveraddress"];
+
+        /// <summary>
+        /// vHost
+        /// </summary>
+        public static string VirtualHost = ConfigurationManager.AppSettings["virtualhost"];
+        /// <summary>
+        /// 访问用户
+        /// </summary>
+        public static string UserName = ConfigurationManager.AppSettings["username"];
+        /// <summary>
+        /// 访问密码
+        /// </summary>
+        public static string Password = ConfigurationManager.AppSettings["password"];
+    }
+}
