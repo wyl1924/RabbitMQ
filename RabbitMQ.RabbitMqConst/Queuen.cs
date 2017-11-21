70f2a92f4a4f3f80dd7c1902a698b09f4fd30499
kmonkey9006 <1714404171@qq.com>
2017-11-21 10:33:38 +0800

    ....

---
+++ RabbitMQ.RabbitMqConst/Queuen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.RabbitMqConst
+{
+    public class Queuen
+    {
+
+
+        /// <summary>
+        /// 队列-库存商城
+        /// </summary>
+        public const string Queuen_Stock_Eshop = "q.Stock.eshop";
+    }
+}
