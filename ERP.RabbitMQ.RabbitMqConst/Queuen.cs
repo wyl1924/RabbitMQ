fdfe9b0e9dce8835e763eb1ef0e7e2a67ac6dc9f
kmonkey9006 <1714404171@qq.com>
2017-11-21 10:25:42 +0800

    RabbitMq:http://www.cnblogs.com/kmonkeywyl/p/7866162.html

---
+++ ERP.RabbitMQ.RabbitMqConst/Queuen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.RabbitMqConst
+{
+    public class Queuen
+    {
+
+
+        /// <summary>
+        /// 队列-库存商城
+        /// </summary>
+        public const string Queuen_Stock_Eshop = "q.Stock.eshop";
+
+        /// <summary>
+        /// 队列--淘宝C  铁血军品行
+        /// </summary>
+        public const string Queuen_Stock_TBC = "q.stock.tbc";
+        /// <summary>
+        /// 队列--淘宝b 铁血户外专营店
+        /// </summary>
+        public const string Queuen_Stock_TBB = "q.stock.tbb";
+        /// <summary>
+        /// 队列--龙牙B  龙牙旗舰店
+        /// </summary>
+        public const string Queuen_Stock_LYB = "q.stock.lyb";
+        /// <summary>
+        /// 队列--京东
+        /// </summary>
+        public const string Queuen_Stock_JD = "q.stock.jd";
+
+    }
+}
